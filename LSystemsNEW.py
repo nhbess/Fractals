26af9d48cf82dd16681cdd0b8382853dc77b7b76
NB <nhbessone@gmail.com>
2024-09-14 16:07:22 +0200

    mmmmmm

--- LSystemsNEW.py
+++ LSystemsNEW.py
@@ -10,13 +10,13 @@ from scipy.ndimage import convolve
 from tqdm import tqdm
 import Visuals
 
-class Board:
-    def __init__(self, n:int, m:int, n_production_rules:int = 2) -> None:
+class LS:
+    def __init__(self, n:int, m:int, n_production_rules:int = 2, production_rules = None) -> None:
+        if production_rules is not None:    self.P = production_rules
+        else:                               self.P = self._production_rules(n_production_rules)
         
-        self.P = self._production_rules(n_production_rules)        
         self.B = np.zeros((n,m), dtype=int)
         self.B[n//2, m//2] = 1
-
         self.data = []
 
 
@@ -31,7 +31,7 @@ class Board:
             for j in range(1, grid_cols+1):
                 sub_grid = padded_grid[i:i + pattern_rows, j:j + pattern_cols] 
                 if np.array_equal(sub_grid, pattern):
-                    M[i - 1, j - 1] = 1        
+                    M[i - 1, j - 1] = 1
         
         return M
 
@@ -46,40 +46,46 @@ class Board:
             reactants = np.random.randint(0, 2, (3, 3))
             products = np.random.randint(0, 2, (3, 3))
             P.append([reactants, products])
+
         return P
     
     def update(self) -> None:
-        S = self.B.copy()  
-        print(f'S\n{S}')      
-
+        S = self.B.copy()
         for rule in self.P:
             reactant, product = rule
-            print(f'reactant\n{reactant}')
+            reactant = reactant.clip(0, 1)
+            reactant = np.where(reactant > 0, 0, reactant)
             M = self._find_pattern(S, reactant)
-            print(f'pattern\n{M}')
+            if sum(sum(M)) == 0:
+                continue
+            #print(f'pattern\n{M}')
+            
             N = convolve(M, product, mode='wrap')
             self.B[N == 1] = 1
+            # where N == 1, set to 1 in B where N == 0, leave deduct 1 in B
+            #self.B[N == 0] = np.where(self.B[N == 0] == 0, 0, self.B[N == 0] - 1)
+            #self.B = np.where(N == 1, 1, self.B)
+            #self.B = np.where(N == 0, np.where(self.B == 0, 0, self.B - 1), self.B)
+            #self.B = np.clip(self.B, 0, 1)
 
         self.data.append(self.B.copy())
 
             
 
 if __name__ == '__main__':
-    seed = np.random.randint(0, 100000000) 
-    seed = 0
-    np.random.seed(seed)
-    print(f'Seed: {seed}')
-    ratio = 16/9
-    Y = 50
-    X = Y   #int(Y/ratio)
-    Z = 3
-    RUNS = X
-    N_PRODUCTION_RULES = 0
-    for run in range(1):    
-        b = Board(X, Y, N_PRODUCTION_RULES)
-        #for i in tqdm(range(RUNS)):
-        for i in range(RUNS):
-            b.update()
-
-        data = b.data
-        Visuals.create_visualization_grid(data, filename=f'Test', duration=100, gif=True, video=False)
+    pass
+    #seed = np.random.randint(0, 100000000) 
+    #
+    #np.random.seed(seed)
+    #print(f'Seed: {seed}')
+    #Y = 50
+    #X = Y   #int(Y/ratio)
+    #RUNS = X
+    #N_PRODUCTION_RULES = 100
+    #for run in range(1):    
+    #    b = Board(X, Y, N_PRODUCTION_RULES)
+    #    for i in range(Y*2):
+    #        b.update()
+#
+    #    data = b.data
+    #    Visuals.create_visualization_grid(data, filename=f'Test', duration=100, gif=True, video=False)
